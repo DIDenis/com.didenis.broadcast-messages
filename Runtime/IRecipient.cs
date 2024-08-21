91db7f5b066ae02f48165389bc12a322c4cd5220
Denis Dmitriev <denisdmitriev7851@gmail.com>
2024-08-21 16:40:33 +0400

    multi-message recipients added

--- Runtime/IRecipient.cs
+++ Runtime/IRecipient.cs
@@ -6,4 +6,38 @@
 
     }
 
+
+    public interface IRecipient<in TMessage1, in TMessage2> where TMessage1 : struct where TMessage2 : struct {
+
+        public void GetMessage (TMessage1 message);
+        public void GetMessage (TMessage2 message);
+
+    }
+
+
+    public interface IRecipient<in TMessage1, in TMessage2, in TMessage3>
+        where TMessage1 : struct
+        where TMessage2 : struct
+        where TMessage3 : struct {
+
+        public void GetMessage (TMessage1 message);
+        public void GetMessage (TMessage2 message);
+        public void GetMessage (TMessage3 message);
+
+    }
+
+
+    public interface IRecipient<in TMessage1, in TMessage2, in TMessage3, in TMessage4>
+        where TMessage1 : struct
+        where TMessage2 : struct
+        where TMessage3 : struct
+        where TMessage4 : struct {
+
+        public void GetMessage (TMessage1 message);
+        public void GetMessage (TMessage2 message);
+        public void GetMessage (TMessage3 message);
+        public void GetMessage (TMessage4 message);
+
+    }
+
 }
--- Runtime/Orchestrator.cs
+++ Runtime/Orchestrator.cs
@@ -13,15 +13,53 @@ namespace BroadcastMessages {
         /// Adds subscriber to sending
         /// </summary>
         /// <param name="recipient">Subscriber with parameters</param>
-        /// <param name="oneTimeSubscription"></param>
-        /// <typeparam name="TMessage">Type of message for sending to subscribers</typeparam>
+        /// <param name="oneTimeSubscription">  </param>
         public static void Subscribe<TMessage> (IRecipient<TMessage> recipient, bool oneTimeSubscription = false)
             where TMessage : struct {
-            if (Subscribers<TMessage>.Sending)
-                Subscribers<TMessage>.OnCompleteSending +=
-                    () => Subscribers<TMessage>.Add(recipient, oneTimeSubscription);
-            else
-                Subscribers<TMessage>.Add(recipient, oneTimeSubscription);
+            Subscribe<TMessage>(recipient.GetMessage, oneTimeSubscription);
+        }
+
+
+        /// <inheritdoc cref="Subscribe{TMessage}(IRecipient{TMessage},bool)"/>
+        public static void Subscribe<TMessage1, TMessage2> (
+            IRecipient<TMessage1, TMessage2> recipient, bool oneTimeSubscription = false
+        ) where TMessage1 : struct where TMessage2 : struct {
+            Subscribe<TMessage1>(recipient.GetMessage, oneTimeSubscription);
+            Subscribe<TMessage2>(recipient.GetMessage, oneTimeSubscription);
+        }
+
+
+        /// <inheritdoc cref="Subscribe{TMessage}(IRecipient{TMessage},bool)"/>
+        public static void Subscribe<TMessage1, TMessage2, TMessage3> (
+            IRecipient<TMessage1, TMessage2, TMessage3> recipient, bool oneTimeSubscription = false
+        ) where TMessage1 : struct where TMessage2 : struct where TMessage3 : struct {
+            Subscribe<TMessage1>(recipient.GetMessage, oneTimeSubscription);
+            Subscribe<TMessage2>(recipient.GetMessage, oneTimeSubscription);
+            Subscribe<TMessage3>(recipient.GetMessage, oneTimeSubscription);
+        }
+
+
+        /// <inheritdoc cref="Subscribe{TMessage}(IRecipient{TMessage},bool)"/>
+        public static void Subscribe<TMessage1, TMessage2, TMessage3, TMessage4> (
+            IRecipient<TMessage1, TMessage2, TMessage3, TMessage4> recipient, bool oneTimeSubscription = false
+        ) where TMessage1 : struct where TMessage2 : struct where TMessage3 : struct where TMessage4 : struct {
+            Subscribe<TMessage1>(recipient.GetMessage, oneTimeSubscription);
+            Subscribe<TMessage2>(recipient.GetMessage, oneTimeSubscription);
+            Subscribe<TMessage3>(recipient.GetMessage, oneTimeSubscription);
+            Subscribe<TMessage4>(recipient.GetMessage, oneTimeSubscription);
+        }
+
+
+        public static void Subscribe<TMessage> (Action<TMessage> recipientChannel, bool oneTimeSubscription = false)
+            where TMessage : struct {
+            if (Channel<TMessage>.Sending) {
+                Channel<TMessage>.OnCompleteSending += () => {
+                    Channel<TMessage>.Add(recipientChannel, oneTimeSubscription);
+                };
+            }
+            else {
+                Channel<TMessage>.Add(recipientChannel, oneTimeSubscription);
+            }
         }
 
 
@@ -29,12 +67,45 @@ namespace BroadcastMessages {
         /// Removes subscriber from sending
         /// </summary>
         /// <param name="recipient">Subscriber with parameters</param>
-        /// <typeparam name="TMessage">Type of message for sending to subscribers</typeparam>
         public static void Unsubscribe<TMessage> (IRecipient<TMessage> recipient) where TMessage : struct {
-            if (Subscribers<TMessage>.Sending)
-                Subscribers<TMessage>.OnCompleteSending += () => Subscribers<TMessage>.Remove(recipient);
+            Unsubscribe<TMessage>(recipient.GetMessage);
+        }
+
+
+        /// <inheritdoc cref="Unsubscribe{TMessage}(IRecipient{TMessage})"/>
+        public static void Unsubscribe<TMessage1, TMessage2> (IRecipient<TMessage1, TMessage2> recipient)
+            where TMessage1 : struct where TMessage2 : struct {
+            Unsubscribe<TMessage1>(recipient.GetMessage);
+            Unsubscribe<TMessage2>(recipient.GetMessage);
+        }
+
+
+        /// <inheritdoc cref="Unsubscribe{TMessage}(IRecipient{TMessage})"/>
+        public static void Unsubscribe<TMessage1, TMessage2, TMessage3> (
+            IRecipient<TMessage1, TMessage2, TMessage3> recipient
+        ) where TMessage1 : struct where TMessage2 : struct where TMessage3 : struct {
+            Unsubscribe<TMessage1>(recipient.GetMessage);
+            Unsubscribe<TMessage2>(recipient.GetMessage);
+            Unsubscribe<TMessage3>(recipient.GetMessage);
+        }
+
+
+        /// <inheritdoc cref="Unsubscribe{TMessage}(IRecipient{TMessage})"/>
+        public static void Unsubscribe<TMessage1, TMessage2, TMessage3, TMessage4> (
+            IRecipient<TMessage1, TMessage2, TMessage3, TMessage4> recipient
+        ) where TMessage1 : struct where TMessage2 : struct where TMessage3 : struct where TMessage4 : struct {
+            Unsubscribe<TMessage1>(recipient.GetMessage);
+            Unsubscribe<TMessage2>(recipient.GetMessage);
+            Unsubscribe<TMessage3>(recipient.GetMessage);
+            Unsubscribe<TMessage4>(recipient.GetMessage);
+        }
+
+
+        public static void Unsubscribe<TMessage> (Action<TMessage> recipientChannel) where TMessage : struct {
+            if (Channel<TMessage>.Sending)
+                Channel<TMessage>.OnCompleteSending += () => Channel<TMessage>.Remove(recipientChannel);
             else
-                Subscribers<TMessage>.Remove(recipient);
+                Channel<TMessage>.Remove(recipientChannel);
         }
 
 
@@ -43,31 +114,30 @@ namespace BroadcastMessages {
         /// </summary>
         /// <param name="message"></param>
         public static void Send<TMessage> (TMessage message = default) where TMessage : struct {
-            Subscribers<TMessage>.Send(message);
+            Channel<TMessage>.Send(message);
         }
-
 
 
         /// <summary>
         /// Keeps in itself subscribers with patameters
         /// </summary>
         /// <typeparam name="TMessage">Тип сообщения, отправляемый подписчикам</typeparam>
-        private static class Subscribers<TMessage> where TMessage : struct {
+        private static class Channel<TMessage> where TMessage : struct {
 
-            private static readonly List<(IRecipient<TMessage>, bool)> Data = new();
             public static event Action OnCompleteSending;
             public static bool Sending { get; private set; }
+            private static readonly List<(Action<TMessage>, bool)> s_data = new();
 
 
-            public static void Add (IRecipient<TMessage> recipient, bool oneTimeSubscription) {
-                Data.Add((recipient, oneTimeSubscription));
+            public static void Add (Action<TMessage> recipient, bool oneTimeSubscription) {
+                s_data.Add((recipient, oneTimeSubscription));
             }
 
 
-            public static void Remove (IRecipient<TMessage> recipient) {
-                foreach ((IRecipient<TMessage>, bool) tuple in Data) {
+            public static void Remove (Action<TMessage> recipient) {
+                foreach ((Action<TMessage>, bool) tuple in s_data) {
                     if (tuple.Item1 == recipient) {
-                        Data.Remove(tuple);
+                        s_data.Remove(tuple);
                         break;
                     }
                 }
@@ -75,18 +145,18 @@ namespace BroadcastMessages {
 
 
             public static void Send (TMessage message) {
-                Data.RemoveAll(data =>
-                    data.Item1 == null || data.Item1 is Object unityRecipient && unityRecipient == null
+                s_data.RemoveAll(data =>
+                    data.Item1.Target == null || data.Item1.Target is Object unityRecipient && unityRecipient == null
                 );
 
                 Sending = true;
 
-                foreach ((IRecipient<TMessage> recipient, bool _) in Data)
-                    recipient.GetMessage(message);
+                foreach ((Action<TMessage> recipientChannel, bool _) in s_data)
+                    recipientChannel(message);
 
-                for (var i = 0; i < Data.Count; i++)
-                    if (Data[i].Item2)
-                        Data.RemoveAt(i);
+                for (var i = 0; i < s_data.Count; i++)
+                    if (s_data[i].Item2)
+                        s_data.RemoveAt(i);
 
                 Sending = false;
 
